a1297914dabf48dbfbe097d973d28a5698c85285
Hyune-c <choi8608@gmail.com>
2023-10-20 00:40:28 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # practice-spring-data-redis
-[practice] spring-data-redis 활용하기
+
+### feature
+
+- V 코드 (UUID 난수코드) 생성 및 저장
+- 동일한 userId 로 사용 불가능한 V 코드를 사용하는 경우 방어로직
+    - 사용자별 사용 실패 누적 횟수는 10분간 유지
+    - 사용자별 누적 5회 사용 실패시 10분간 V 코드 사용 시도 불가
+    - (optional) V 코드 사용 성공시 실패 횟수 초기화
+
+### environment
+
+- spring boot 3.1.5 + kotlin
---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val kotestVersion: String by project
+    val kotestExtVersion: String by project
+
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // kotest
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("io.kotest:kotest-extensions-now:${kotestVersion}")
+    implementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtVersion")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
